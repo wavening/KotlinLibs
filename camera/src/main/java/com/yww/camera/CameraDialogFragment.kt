c89ecd9d287dd54c5694159d212e6f604aa55698
wavening <1937487736@qq.com>
2019-05-13 17:11:06 +0800

    commit again

--- camera/src/main/java/com/yww/camera/CameraDialogFragment.kt
+++ camera/src/main/java/com/yww/camera/CameraDialogFragment.kt
@@ -1,7 +1,25 @@
-package com.yww.camera.stragedy
+package com.yww.camera
+
+import com.yww.base.BaseDialogFragment
+import com.yww.camera.stragedy.CameraAction
 
 /**
  * @author  WAVENING
  */
-class CameraDialogFragment {
+class CameraDialogFragment:BaseDialogFragment(),CameraAction {
+    override fun startPreview() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun stopPreview() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun takePicture() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun close() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
